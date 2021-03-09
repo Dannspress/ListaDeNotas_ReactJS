e41e53273fd1e6c7951701a56ffa205a5e3ccbd8
Daniel Walter <danielwalterwork@gmail.com>
2021-03-09 13:07:44 -0300

    Aplicando CSS

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import ListaDeNotas from './components/listaDeNotas'
-import FormularioCadastro from './components/formularioCadastro'
+import ListaDeNotas from './components/listaDeNotas/listaDeNotas'
+import FormularioCadastro from './components/formularioCadastro/formularioCadastro'
+import "./assets/app.css";
+import "./assets/index.css"
+
 
 class App extends Component {
   render() {
     return (
-      <section>
+      <section className='conteudo'>
         <FormularioCadastro/>
         <ListaDeNotas/>
       </section>
